5f5e44b03565c046b6f307fd03cbe2cfd25f6259
CodeRaido <coderaido@gmail.com>
2025-05-09 07:37:02 +0200

    :sparkles: Add database manager
    Add database_manager.py file with class to allow database connections

---
+++ cuelebre_vault/database/database_manager.py
@@ -0,0 +1,31 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
+from sqlalchemy import URL, Engine
+from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine, create_async_engine, async_sessionmaker
+
+
+class DatabaseManager:
+
+    def __init__(self, url: str | URL, **kwargs):
+
+        self.__engine: AsyncEngine = create_async_engine(url, **kwargs)
+        self.__session_factory: async_sessionmaker = async_sessionmaker(self.__engine, expire_on_commit=True)
+
+    @property
+    def async_engine(self) -> AsyncEngine:
+        return self.__engine
+
+    @property
+    def sync_engine(self) -> Engine:
+        return self.__engine.sync_engine
+
+    @property
+    def async_session_factory(self) -> async_sessionmaker[AsyncSession]:
+        return self.__session_factory
+
+    @asynccontextmanager
+    async def get_async_session(self) -> AsyncGenerator[AsyncSession, ...]:
+        async with self.__session_factory() as session:
+            session: AsyncSession
+            yield session
