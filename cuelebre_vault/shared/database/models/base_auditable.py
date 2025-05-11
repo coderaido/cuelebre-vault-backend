130921674ea19cbb2a1d82614d49cfbb025d9477
CodeRaido <coderaido@gmail.com>
2025-05-11 13:12:46 +0200

    :bug: Fix server defaults and paths in base auditable model

--- cuelebre_vault/shared/database/models/base_auditable.py
+++ cuelebre_vault/shared/database/models/base_auditable.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from sqlalchemy import func, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, declarative_mixin
 
-from cuelebre_vault.database.models.base import Base
+from cuelebre_vault.shared.database.models.base import Base
 
 
 @declarative_mixin
@@ -11,13 +11,13 @@ class BaseAuditable(Base):
     __abstract__ = True
 
     created_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), server_default=func.now, nullable=False, comment="Creation date"
+        DateTime(timezone=True), server_default=func.now(), nullable=False, comment="Creation date"
     )
 
     updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
-        server_default=func.now,
-        server_onupdate=func.now,
+        server_default=func.now(),
+        server_onupdate=func.now(),
         nullable=False,
         comment="Last row update",
     )
